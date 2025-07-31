34468267ed21cf2e2f94b21aeea5ab2a99a76afc
Gustavo Lopes <guhrlopes999@gmail.com>
2025-07-31 09:40:13 -0300

    Reformulando funções

--- JavaScript com Nodejs/src/cli.js
+++ JavaScript com Nodejs/src/cli.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import trataErros from './erros/funcoesErro.js';
 import { contaPalavras } from './index.js';
+import { montaSaidaArquivo } from './helpers.js'
 
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
@@ -16,13 +17,29 @@ fs.readFile(link, 'utf-8', (erro, texto) => {
     }
 })
 
-async function criaESalvaArquivo(listaPalavras, endereco) {
+//Usando async/await para fazer a função assíncrona
+// async function criaESalvaArquivo(listaPalavras, endereco) {
+//     const arquivoNovo = `${endereco}/resultado.txt`;
+//     const textoPalavras = JSON.stringify(listaPalavras);
+//     try {
+//        await  fs.promises.writeFile(arquivoNovo, textoPalavras);
+//         console.log('Arquivo criado!')
+//     } catch (erro) {
+//         throw erro;
+//     }
+// }
+
+//Usando then para fazer a função assíncrona
+function criaESalvaArquivo(listaPalavras, endereco) {
     const arquivoNovo = `${endereco}/resultado.txt`;
-    const textoPalavras = JSON.stringify(listaPalavras);
-    try {
-       await  fs.promises.writeFile(arquivoNovo, textoPalavras);
-        console.log('Arquivo criado!')
-    } catch (erro) {
-        throw erro;
-    }
+    const textoPalavras = montaSaidaArquivo(listaPalavras)
+    
+    fs.promises.writeFile(arquivoNovo, textoPalavras)
+    .then(() => {
+        console.log('Arquivo criado');
+    })
+    .catch((erro) => {
+      throw erro;  
+    })
+    .finally(() => console.log('Operação finalizada'));
 }
