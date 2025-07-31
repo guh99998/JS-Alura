39331b3a697d5d8d5d62e5d3238cbede8dece035
Gustavo Lopes <guhrlopes999@gmail.com>
2025-07-31 10:17:49 -0300

    implementando bibliotecas

--- JavaScript com Nodejs/src/cli.js
+++ JavaScript com Nodejs/src/cli.js
@@ -1,21 +1,49 @@
 import fs from 'fs';
+import path from 'path';
 import trataErros from './erros/funcoesErro.js';
 import { contaPalavras } from './index.js';
 import { montaSaidaArquivo } from './helpers.js'
+import { Command } from 'commander';
 
-const caminhoArquivo = process.argv;
-const link = caminhoArquivo[2];
-const endereco = caminhoArquivo[3];
+const program = new Command();
 
-fs.readFile(link, 'utf-8', (erro, texto) => {
-    try {
-        if (erro) throw erro
-        const resultado = contaPalavras(texto);
-        criaESalvaArquivo(resultado, endereco);
-    } catch(erro) {
-        trataErros(erro);
-    }
-})
+program
+    .version('0.0.1')
+    .option('-t, --texto <string>', 'caminho do texto a ser processado')
+    .option('-d, --destino <string>', 'caminho da pasta onde salvar o arquivo de resultados')
+    .action((options) => {
+        const { texto, destino } = options;
+
+        if (!texto || !destino) {
+            console.error('erro: favor inserir caminho de origem e destino')
+            program.help();
+            return;
+        }
+
+        const caminhoTexto = path.resolve(texto);
+        const caminhoDestino = path.resolve(destino);
+
+        try {
+            processaArquivo(caminhoTexto, caminhoDestino);
+            console.log('Texto processado com sucesso');
+            
+        } catch (erro) {
+            console.log('Ocorreu um erro no processamento!', erro);
+        }
+    })
+program.parse();
+
+function processaArquivo(texto, destino) {
+    fs.readFile(texto, 'utf-8', (erro, texto) => {
+        try {
+            if (erro) throw erro
+            const resultado = contaPalavras(texto);
+            criaESalvaArquivo(resultado, destino);
+        } catch (erro) {
+            trataErros(erro);
+        }
+    })
+}
 
 //Usando async/await para fazer a função assíncrona
 // async function criaESalvaArquivo(listaPalavras, endereco) {
@@ -33,13 +61,13 @@ fs.readFile(link, 'utf-8', (erro, texto) => {
 function criaESalvaArquivo(listaPalavras, endereco) {
     const arquivoNovo = `${endereco}/resultado.txt`;
     const textoPalavras = montaSaidaArquivo(listaPalavras)
-    
+
     fs.promises.writeFile(arquivoNovo, textoPalavras)
-    .then(() => {
-        console.log('Arquivo criado');
-    })
-    .catch((erro) => {
-      throw erro;  
-    })
-    .finally(() => console.log('Operação finalizada'));
+        .then(() => {
+            console.log('Arquivo criado');
+        })
+        .catch((erro) => {
+            throw erro;
+        })
+        .finally(() => console.log('Operação finalizada'));
 }
