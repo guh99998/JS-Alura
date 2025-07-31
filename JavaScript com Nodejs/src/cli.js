5e2204889625d5d48240294b58e0cf948d84e9e0
Gustavo Lopes <guhrlopes999@gmail.com>
2025-07-31 11:13:37 -0300

    biblioteca para decoração do terminal

--- JavaScript com Nodejs/src/cli.js
+++ JavaScript com Nodejs/src/cli.js
@@ -4,6 +4,7 @@ import trataErros from './erros/funcoesErro.js';
 import { contaPalavras } from './index.js';
 import { montaSaidaArquivo } from './helpers.js'
 import { Command } from 'commander';
+import chalk from 'chalk';
 
 const program = new Command();
 
@@ -15,7 +16,7 @@ program
         const { texto, destino } = options;
 
         if (!texto || !destino) {
-            console.error('erro: favor inserir caminho de origem e destino')
+            console.error(chalk.red('erro: favor inserir caminho de origem e destino'))
             program.help();
             return;
         }
@@ -25,10 +26,10 @@ program
 
         try {
             processaArquivo(caminhoTexto, caminhoDestino);
-            console.log('Texto processado com sucesso');
+            console.log(chalk.green('Texto processado com sucesso'));
             
         } catch (erro) {
-            console.log('Ocorreu um erro no processamento!', erro);
+            console.log(chalk.red('Ocorreu um erro no processamento!'), erro);
         }
     })
 program.parse();
